df2de3cf83af90296c493adc11bbe4bbb1c38ea4
qwe5507 <apvmffkdls@naver.com>
2022-05-20 22:50:31 +0900

    서블릿 예외 처리 - 오류 페이지 작동 원리

--- src/main/java/study/exception/servlet/ServletExController.java
+++ src/main/java/study/exception/servlet/ServletExController.java
@@ -4,12 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Slf4j
 @Controller
 public class ServletExController {
+
 
     @GetMapping("/error-ex")
     public void errorEx() {
@@ -25,4 +27,6 @@ public class ServletExController {
     public void error500(HttpServletResponse response) throws IOException {
         response.sendError(500);
     }
+
+
 }
